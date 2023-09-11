854d2c9c13ce5f7969d312a4f8866e9b273ed0fe
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2023-09-12 02:18:07 +0530

    add more file

--- a.java
+++ a.java
@@ -1,5 +1,7 @@
-class a{
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+class a {
+
+  public static void main(String[] args) {
+    System.out.print("Hello");
+    System.out.println("World");
+  }
 }
